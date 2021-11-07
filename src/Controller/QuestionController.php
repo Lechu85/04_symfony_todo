ab3b7ba695b59c41d67c2c9ebbf3e70e6adcc019
Lechu85 <50577262+Lechu85@users.noreply.github.com>
2021-11-07 21:59:28 +0100

    Dodałem pierwsz yszablon twiga

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,35 @@
 
 namespace App\Controller;
 
-class QuestionController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('Pierwszy tekst w aplikacji :) ');
+    }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+        $answers = [
+            'Make sure your cat is sitting',
+            'Honestly, I like furry shoes better than my cat',
+            'Maybe... Try saying the spell backwards',
+        ];
+
+        //render zwraca objekt Component\HttpFoundation\Response
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-',' ', $slug)),
+            'answers' => $answers,
+        ]);
+    }
 }
